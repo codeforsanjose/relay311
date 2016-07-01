0a039ebb083373e81392809968ad4d5da066bcd9
Karen Calpo <minnkc@gmail.com>
2016-06-30 18:19:22 -0700

    Added searchbar

--- www/js/controllers/category.controller.js
+++ www/js/controllers/category.controller.js
@@ -1,20 +1,69 @@
+
+
 angular.module('open311.controllers')
 .controller('CategoryCtrl', ['$scope', '$ionicHistory', '$ionicPlatform', 'API', 'App',
 function($scope, $ionicHistory, $ionicPlatform, API, App) {
   var coords = { lat: 37.339244, lng: -121.883638 };
+  var data;
+
 
   $ionicPlatform.ready(function() {
     API.getCategories(coords.lat, coords.lng).then(function(response) {
-      $scope.data = response.data;
+      data = response.data;
+      $scope.filteredData = _.clone(response.data);
+      
     });
   });
+
+  $scope.searchList = function (ev) {
+    var items = data;
+    var val = ev.target.value;
+    $scope.filteredItems = items.filter(function(item) {
+        var selection = item.service_name.toLowerCase();
+        return (_.startsWith(selection, val));
+    });   
+  }
 
   $scope.selectItem = function (category) {
     var requestObj = App.getIssue();
     requestObj.category = category;
+    console.log(category);
     App.setIssue(requestObj);
-    
+    console.log(App.setIssue(requestObj));    
     $ionicHistory.goBack();
   }
 
+
+
+
+  // function searchPage() {
+  //   this.searchQuery = '';
+  //   this.initializeItems();
+  // }
+
+  // searchPage.prototype.initializeItems = function() {
+  //   this.items = [
+  //     'Angular 1.x',
+  //     'Angular 2',
+  //     'ReactJS',
+  //     'EmberJS',
+  //     'Meteor',
+  //     'Typescript',
+  //     'Dart',
+  //     'CoffeeScript'
+  //   ];
+  // }
+
+  // searchPage.prototype.getItems = function(ev){
+  //   this.initializeItems();
+  //   let val = ev.target.value;
+    
+  //   if (val && val.trim() != '') {
+  //       this.items = this.items.filter((item) => {
+  //         return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+  //       })
+  //     }
+  // }
+
 }]);
+ 
