5181fc6c6d1a1c4e44f41edb950f90d33525336a
arichen <arichenyc@gmail.com>
2016-09-08 19:15:16 -0700

    1. Fix iPhone not loading menu icons. 2. Fix status bar color to be visible

--- www/js/controllers/home.controller.js
+++ www/js/controllers/home.controller.js
@@ -1,6 +1,14 @@
 angular.module('open311.controllers', [])
-.controller('HomeTabCtrl', function($scope, $ionicModal, $state) {
+.controller('HomeTabCtrl', ['$scope', '$ionicModal', '$state', '$ionicPlatform', function($scope, $ionicModal, $state, $ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.StatusBar) {
+      if (ionic.Platform.isIOS()) {
+        StatusBar.styleDefault();
+      }
+    }
+  });
+
   $scope.onMenuClick = function(state) {
     $state.go('tabs.' + state);
   }
-});
+}]);
